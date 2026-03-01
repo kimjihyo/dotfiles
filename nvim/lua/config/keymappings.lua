830a4f900da35d0956e3388bf6177f85ca3e8d62
kimjihyo <kimjihyo0325@gmail.com>
2026-03-01 16:13:55 +0900

    feat: add neo-tree toggle keybinding (<leader>e)

--- nvim/lua/config/keymappings.lua
+++ nvim/lua/config/keymappings.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>")
 
 vim.keymap.set("n", "<F5>", function()
 	vim.cmd("write")
