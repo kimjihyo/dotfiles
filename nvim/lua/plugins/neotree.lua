dd69df2314f50efe5406fe5e4898492fcd8aa770
kimjihyo <kimjihyo0325@gmail.com>
2026-03-19 19:36:35 +0900

    feat: neo-tree에 window-picker 플러그인 연동
    nvim-window-picker를 추가하여 파일 열기 시 대상 창을 선택할 수 있도록 함.
    enter/s/S 키맵을 window_picker 버전으로 변경하고,
    터미널/trouble/quickfix 창은 대체 대상에서 제외

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -5,8 +5,30 @@ return {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 		"MunifTanjim/nui.nvim",
+		{
+			"s1n7ax/nvim-window-picker",
+			version = "2.*",
+			opts = {
+				hint = "floating-big-letter",
+				filter_rules = {
+					autoselect_one = true,
+					bo = {
+						filetype = { "neo-tree", "neo-tree-popup", "notify" },
+						buftype = { "terminal", "quickfix" },
+					},
+				},
+			},
+		},
 	},
 	opts = {
+		open_files_do_not_replace_types = { "terminal", "trouble", "qf" },
+		window = {
+			mappings = {
+				["<cr>"] = "open_with_window_picker",
+				["s"] = "vsplit_with_window_picker",
+				["S"] = "split_with_window_picker",
+			},
+		},
 		filesystem = {
 			use_libuv_file_watcher = true,
 			filtered_items = {
